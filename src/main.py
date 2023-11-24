1421b55fdc3555b26e7c8fa95ec3a7d43bc2db1e
Petros Kolyvas <code@petros.io>
2023-11-24 10:56:21 -0400

    A little more work

--- src/main.py
+++ src/main.py
@@ -25,9 +25,9 @@ longitude = -65.97948
 #         "X-RapidAPI-Host": "moon-phase.p.rapidapi.com"
 #     }
 
-#     response = requests.get(url, headers=headers, params=querystring)
+#     moons_json_raw = requests.get(url, headers=headers, params=querystring)
     
-#     return  json.loads(response.json())
+#     return  json.loads(moons_json_raw.json())
 
 # def get_tide_data(latitude, longitude):
 
@@ -43,21 +43,22 @@ longitude = -65.97948
 #         "x-marea-api-token": os.environ.get('MAREA_API_TOKEN'),
 #     }
 
-#     response = requests.get(url, headers=headers, params=querystring)
+#     tides_json_raw = requests.get(url, headers=headers, params=querystring)
     
-#     with open('current_tides.json', 'w') as json_file:
-#         json.dump(response.json(), json_file )
-    
-#     return  json.loads(response.json())
+#     return  json.loads(tides_json_raw.json())
 
+# This replaces the request from the API for testing
+# using a local file which matches the Moons API response.
 with open('moon_sample.json') as user_file:
-    moons = user_file.read()
+    moons_json_raw = user_file.read()
 
-moon_data = json.loads(moons)
+moon_data = json.loads(moons_json_raw)
 
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS current_moon (phase REAL, timestamp TEXT)")
 
+# Our moon class really on needs the name of the next phase and 
+# the timestamp of that phase.
 class Moon:
     def __init__(self, moon, timestamp):
         self.moon = moon
@@ -71,34 +72,41 @@ class Moon:
 
 moons = []
 
+# Here we iterate over the next moon phases to create an 
+# object for each moon phase with a timestamp
 for moon in moon_data["moon_phases"]:
-    addmoon = Moon(moon, moon[1][0])
-    moons.append(addmoon)
+    moon_phase = Moon(moon, moon_data["moon_phases"][moon]["next"]["timestamp"])
+    moons.append(moon_phase)
 
+# Here we sort them such that we create a list which will
+# allow us to use the next moon, and, following that,
+# retain a list of subsequent moons in case internet connectivity
+# is limited. We should be able to remove items from the front of the 
+# list when they're in the past
 moons_sorted = sorted(moons)
 
-print(moons[0].timestamp)
-print(moons_sorted[0].moon)
-
-def set_mask_position(phase):
+# This simple function takes the phase percentage and
+# will calculate the number of motor steps to move the
+# mask. The moto is a 200 step motor or 1.8 degrees per
+# step. That gives us clear correlation with the four
+# moon phases: new (0), first quarter (50 steps), etc. etc.
+def set_moon_mask_position(phase_percentage):
     steps = 200
     position = phase * steps
     return int(position)
 
-print("Current phase: "+str(moon_data["moon"]["phase"])) 
-print("Days until next new moon: "+str(moon_data["moon_phases"]["new_moon"]["next"]["days_ahead"]))
-print("Steps to set moon mask: "+str(set_mask_position(float(moon_data["moon"]["phase"]))))
+# Sanity check data structures and access
+#########################################
+# print("Current phase: "+str(moon_data["moon"]["phase"])) 
+# print("Days until next new moon: "+str(moon_data["moon_phases"]["new_moon"]["next"]["days_ahead"]))
+# print("Steps to set moon mask: "+str(set_mask_position(float(moon_data["moon"]["phase"]))))
 
-
-
-
-
-
-
+# This replaces the request from the API for testing
+# using a local file which matches the tides API response.
 with open('current_tides.json') as user_file:
-    tides = user_file.read()
+    tides_json_raw = user_file.read()
     
-tide_data = json.loads(tides)
+tide_data = json.loads(tides_json_raw)
 
 cursor.execute("CREATE TABLE IF NOT EXISTS tides (tide TEXT, date TEXT, height REAL, timestamp INT)")
 
@@ -117,6 +125,3 @@ class Tide:
 #     if len(tide_list) != 1:
 #         current_index = len(tide_list)-1
 #         tide_list[current_index-1].next_tide = tide_list[current_index]
-
-
-
