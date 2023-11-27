bf8741c0fcfd88db321482496fc8e51344ec85b4
Petros Kolyvas <code@petros.io>
2023-11-27 17:12:41 -0400

    getting ready to test the motor

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ import json
 import os
 import time
 import threading, queue
-#from adafruit_motorkit import MotorKit
+import board
+from adafruit_motorkit import MotorKit
 
 connection = sqlite3.connect("moons_and_tides.db")
-
-
+kit = MotorKit(i2c=board.I2C())
 
 latitude = 45.38988
 longitude = -65.97948
@@ -32,26 +32,6 @@ motor_resolution = 200
     
 #     return  json.loads(moons_json_raw.json())
 
-# def get_tide_data(latitude, longitude):
-
-#     url = "https://api.marea.ooo/v2/tides"
-
-#     querystring = {
-#         "duration":"1440",
-#         "latitude":latitude,
-#         "longitude":longitude
-#         }
-
-#     headers = {
-#         "x-marea-api-token": os.environ.get('MAREA_API_TOKEN'),
-#     }
-
-#     tides_json_raw = requests.get(url, headers=headers, params=querystring)
-    
-#     return  json.loads(tides_json_raw.json())
-
-# This replaces the request from the API for testing
-# using a local file which matches the Moons API response.
 with open('moon_sample.json') as user_file:
     moons_json_raw = user_file.read()
 
@@ -59,6 +39,7 @@ moon_data = json.loads(moons_json_raw)
 
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS current_moon (phase REAL, timestamp TEXT, percent REAL)")
+cursor.execute("CREATE TABLE IF NOT EXISTS tides (tide TEXT, date TEXT, height REAL, timestamp INT)")
 
 # Our moon class really only needs the name of the next phase and 
 # the timestamp of that phase.
@@ -186,6 +167,7 @@ def moon_worker():
                 # motor increment by current percent
                 current_percent = current_percent+percent_per_step
                 set_moon_mask_position(current_percent)
+                # kit.stepper1.onestep()
                 current_time = time.time()
                 print("incrementing") 
 
@@ -195,47 +177,8 @@ def moon_worker():
 moon_thread = threading.Thread(target=moon_worker)
 moon_thread.start()
 
-
-
 # Sanity check data structures and access
 #########################################
 # print("Current phase: "+str(moon_data["moon"]["phase"])) 
 # print("Days until next new moon: "+str(moon_data["moon_phases"]["new_moon"]["next"]["days_ahead"]))
 # print("Steps to set moon mask: "+str(set_mask_position(float(moon_data["moon"]["phase"]))))
-
-# This replaces the request from the API for testing
-# using a local file which matches the tides API response.
-with open('current_tides.json') as user_file:
-    tides_json_raw = user_file.read()
-    
-tide_data = json.loads(tides_json_raw)
-
-cursor.execute("CREATE TABLE IF NOT EXISTS tides (tide TEXT, date TEXT, height REAL, timestamp INT)")
-
-class Tide:
-    def __init__(self, tide, timestamp, height, next_tide=None) -> None:
-        self.tide = tide
-        self.timestamp = timestamp
-        self.height = height
-        self.next_tide = next_tide
-
-tide_list = []
-
-# for tide in tide_data["extremes"]:
-#     new_tide = Tide(tide["state"], tide["timestamp"], tide["height"])
-#     tide_list.append(new_tide)
-#     if len(tide_list) != 1:
-#         current_index = len(tide_list)-1
-#         tide_list[current_index-1].next_tide = tide_list[current_index]
-
-# tide_queue = queue.Queue()
-
-# def tide_worker():
-#     while True
-#         item = tide_queue.get()
-#         # stuff
-#         tide_queue.done()
-        
-# threading.Thread(target=tide_queue, daemon=True).start()
-
-# tide_queue.join()
---
+++ src/tides.py
@@ -0,0 +1,56 @@
+# def get_tide_data(latitude, longitude):
+
+#     url = "https://api.marea.ooo/v2/tides"
+
+#     querystring = {
+#         "duration":"1440",
+#         "latitude":latitude,
+#         "longitude":longitude
+#         }
+
+#     headers = {
+#         "x-marea-api-token": os.environ.get('MAREA_API_TOKEN'),
+#     }
+
+#     tides_json_raw = requests.get(url, headers=headers, params=querystring)
+    
+#     return  json.loads(tides_json_raw.json())
+
+# This replaces the request from the API for testing
+# using a local file which matches the Moons API response.
+
+# This replaces the request from the API for testing
+# using a local file which matches the tides API response.
+with open('current_tides.json') as user_file:
+    tides_json_raw = user_file.read()
+    
+tide_data = json.loads(tides_json_raw)
+
+
+
+class Tide:
+    def __init__(self, tide, timestamp, height, next_tide=None) -> None:
+        self.tide = tide
+        self.timestamp = timestamp
+        self.height = height
+        self.next_tide = next_tide
+
+tide_list = []
+
+for tide in tide_data["extremes"]:
+    new_tide = Tide(tide["state"], tide["timestamp"], tide["height"])
+    tide_list.append(new_tide)
+    if len(tide_list) != 1:
+        current_index = len(tide_list)-1
+        tide_list[current_index-1].next_tide = tide_list[current_index]
+
+def tide_worker():
+    while True
+        item = tide_queue.get()
+        # stuff
+        tide_queue.done()
+        
+tide_thread = threading.Thread(target=tide_worker)
+tide_thread.start()
+
+# tide_queue.join()
