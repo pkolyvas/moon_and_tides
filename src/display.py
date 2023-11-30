a91f4a614a0b7d1b64d760abb667da3975c28bc1
Petros Kolyvas <code@petros.io>
2023-11-29 20:26:28 -0400

    Menu layout tweaks

--- src/display.py
+++ src/display.py
@@ -49,6 +49,8 @@ display = ST7789(
 )
 
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
+top_row_height = 50
+bottom_row_height = 153
 
 button_a = "Swap"
 button_b = "Calibrate"
@@ -56,10 +58,10 @@ button_x = "Info & Logs"
 button_y = "Back"
 
 while True:
-    draw.text((10,35), button_a, font=font, fill=(255, 255, 255))
-    draw.text((10,153), button_b, font=font, fill=(255, 255, 255))
-    draw.text((200,35), button_x, font=font, fill=(255, 255, 255))
-    draw.text((255,153), button_y, font=font, fill=(255, 255, 255))
+    draw.text((10,top_row_height), button_a, font=font, fill=(255, 255, 255))
+    draw.text((10,bottom_row_height), button_b, font=font, fill=(255, 255, 255))
+    draw.text((200,top_row_height), button_x, font=font, fill=(255, 255, 255))
+    draw.text((255,bottom_row_height), button_y, font=font, fill=(255, 255, 255))
     display.display(buffer)
     time.sleep(1.0 / 60)
 
