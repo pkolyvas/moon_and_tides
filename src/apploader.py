ac49b642c1309804784d2e92f0552a4a7e6ff350
Petros K. <code@petros.io>
2025-12-27 12:17:45 -0400

    Apploader changes

--- src/apploader.py
+++ src/apploader.py
@@ -1,7 +1,18 @@
 import configparser
+import os
+import sys
+
+# Get the directory where this script is located
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Check for command-line argument, otherwise default to script directory
+if len(sys.argv) > 1:
+    CONFIG_PATH = sys.argv[1]
+else:
+    CONFIG_PATH = os.path.join(SCRIPT_DIR, 'app.conf')
 
 config = configparser.ConfigParser()
 config.sections()
 
 # TODO: We could add an if statement here and write the config file using a web UI
-config.read('app.conf')
+config.read(CONFIG_PATH)
